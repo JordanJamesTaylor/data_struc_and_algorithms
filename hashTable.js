5a144197477c16b79c5758a819c72e6d0a9edcb0
JordanJamesTaylor <jordjamestaylor@gmail.com>
2022-10-31 16:50:03 -0400

    Hash Table methods

---
+++ hashTable.js
@@ -0,0 +1,4 @@
+/*
+    One way - key bottles goes in, number 25 comes out. You cannot then put 25 in and get bottles out. It only goes in one way.
+    Deterministic - bottles will always produce 25 once it has been added to the table
+*/ 
